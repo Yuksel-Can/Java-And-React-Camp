ed639ac15d2a16f62f824731184356705ab41bff
Yüksel Can Özdemir <yukselcanozdemir@gmail.com>
2021-05-03 22:25:32 +0300

    version 2

---
+++ javaCamp/lesson3/lesson3/inheritance2/src/inheritance2/CustomerManager.java
@@ -0,0 +1,10 @@
+package inheritance2;
+
+public class CustomerManager {
+
+	public void add(Logger logger) {
+		//musteri ekleme kodlarý
+		System.out.println("Müþteri Eklendi");
+		logger.log();
+	}
+}
---
+++ javaCamp/lesson3/lesson3/inheritance2/src/inheritance2/FileLogger.java
@@ -0,0 +1,9 @@
+package inheritance2;
+
+public class FileLogger extends Logger{
+	@Override
+	public void log() {
+		System.out.println("Dosya'ya Loglandý");
+	}
+
+}
